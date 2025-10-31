029ee1d64199e0a393288d4d70cb1e9bfd9870d9
Aakash Rajbanshi <aakash.r@doohclick.com>
2025-10-31 11:39:05 +0545

    added: theme and components

--- typing_game/src/app/app.routes.ts
+++ typing_game/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { TypingTest } from './pages/typing-test/typing-test';
+import { Leaderboard } from './services/leaderboard';
+import { About } from './pages/about/about';
+import { Themes } from './pages/themes/themes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/typing_test', pathMatch: 'full'},
+    {path: 'home', component: Home},
+    {path: 'typing_test', component: TypingTest},
+    {path: 'leaderboard', component: Leaderboard},
+    {path: 'themes', component: Themes},
+    {path: 'error', component: Error},
+    {path: '**', component: Error},
+];
---
+++ typing_game/src/app/pages/typing-test/typing-test.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-test',
+  imports: [],
+  templateUrl: './typing-test.html',
+  styleUrl: './typing-test.scss',
+})
+export class TypingTest {
+
+}
